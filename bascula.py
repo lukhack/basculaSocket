f91691c4d7e2143efdffc50043f8688fd0597c72
lukhack <juligrva@hotmail.com>
2021-05-07 14:48:34 -0500

    hw 7052021

--- bascula.py
+++ bascula.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
 import time
+import requests
 
 # Initializing the flask object
 app = Flask(__name__)
@@ -11,24 +12,29 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on('getSomeData', namespace='/')
+@socketio.on('basculaPeso', namespace='/')
 def handle_my_custom_namespace_event(json):
-    print('received json: ' + str(json))
-    socketio.emit('getSomeData', {'data': 42})
+    #print('received json: ' + str(json))
+    #socketio.emit('basculaPeso', {'data': 42})
+    some_function()
 
 @socketio.on("connect", namespace="/")
 def connect():
+    pass
     # global variable as it needs to be shared
     #global clients
     # emits a message with the user count anytime someone connects
     #some_function()
-    socketio.emit('getSomeData', {'data': 42})
+    #socketio.emit('basculaPeso', {'data': 42})
 
 def some_function():
     while True:
-        print("datos")
-        socketio.emit('getSomeData', {'data': 42})
-        time.sleep(1)
+        x = requests.get('http://192.168.1.41/get_var.cgi?index=99')
+        data = x.json()['value']
+        peso = x = data.split("|")[6]
+        #print("peso: "+peso)
+        socketio.emit('basculaPeso', {'peso': peso})
+        time.sleep(1.1)
 # If you are running it using python <filename> then below command will be used
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=10001)
